8337aaf932e3889b974d83adf0c2c5916b93e687
Fiqri Syah Redha <wibulo27@gmail.com>
2026-07-13 12:11:43 +0700

    chore(web): add auto code splitting to tanstack router

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -1,11 +1,17 @@
 import path from 'node:path';
 import tailwindcss from '@tailwindcss/vite';
-import { TanStackRouterVite } from '@tanstack/router-plugin/vite';
+import { tanstackRouter } from '@tanstack/router-plugin/vite';
 import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-  plugins: [TanStackRouterVite(), tailwindcss(), react()],
+  plugins: [
+    tanstackRouter({
+      autoCodeSplitting: true,
+    }),
+    tailwindcss(),
+    react(),
+  ],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
